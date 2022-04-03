d444f88b1671143ad3ef7d2f34f86d5113edb3d0
dhruv50ae <linkdp2012@gmail.com>
2022-04-03 11:54:18 +0100

    Refactored input handle to anonymous func

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
 
 export default class SearchBar extends Component {
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+  // onInputChange(event) {
+  //   console.log(event.target.value);
+  // }
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input onChange={this.onInputChange} type="text" />
+            <input
+              onChange={(event) => {
+                console.log(event.target.value);
+              }}
+              type="text"
+            />
           </div>
         </form>
       </div>

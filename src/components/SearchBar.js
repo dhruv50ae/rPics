b35c113c74408c43a6b6b6c6cd70442e91981cbc
dhruv50ae <linkdp2012@gmail.com>
2022-04-03 12:01:17 +0100

    Prevented sending form to a backend

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,10 +2,14 @@ import React, { Component } from "react";
 
 export default class SearchBar extends Component {
   state = { term: "" };
+  onSubmitForm(e) {
+    e.preventDefault();
+    console.log(this.state.term);
+  }
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onSubmitForm} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input

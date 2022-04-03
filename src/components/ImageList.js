3061f14c69b2e1dfe7b654ce458b22c861f8b5d4
dhruv50ae <linkdp2012@gmail.com>
2022-04-03 14:26:30 +0100

    ES6 destructured params and img tag updated

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 const ImageList = (props) => {
-  const images = props.images.map((image) => {
-    return <img src={image.urls.regular} />;
+  const images = props.images.map(({ urls, id, description }) => {
+    return <img src={urls.regular} key={id} alt={description} />;
   });
   return <div>{images}</div>;
 };

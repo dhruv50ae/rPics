991117a689837672e4faaa17880cf0208c9d35a7
dhruv50ae <linkdp2012@gmail.com>
2022-04-03 14:35:06 +0100

    Added foundational grid styling

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./ImageList.css";
 
 const ImageList = (props) => {
   const images = props.images.map(({ urls, id, description }) => {
     return <img src={urls.regular} key={id} alt={description} />;
   });
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;

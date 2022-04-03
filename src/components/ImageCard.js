4f073ca998a978f5cbba952b8456dd42f597a301
dhruv50ae <linkdp2012@gmail.com>
2022-04-03 14:53:03 +0100

    Implemented Ref System

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 
-const ImageCard = (props) => {
-  const { urls, description } = props.image;
-  return (
-    <div>
-      <img src={urls.regular} alt={description} />
-    </div>
-  );
-};
-
-export default ImageCard;
+export default class ImageCard extends Component {
+  constructor(props) {
+    super(props);
+    this.imageRef = React.createRef();
+  }
+  componentDidMount() {
+    console.log(this.imageRef);
+  }
+  render() {
+    const { urls, description } = this.props.image;
+    return (
+      <div>
+        <img ref={this.imageRef} src={urls.regular} alt={description} />
+      </div>
+    );
+  }
+}
